4063df7374065aa933a4c1e012d5fd5fac4bf18d
ShyluVishnu <vshylu26@gmail.com>
2017-06-14 23:45:36 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class helllo
+{
+public static void main(String[] args)
+{
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int count=0;
+    while(n!=0)
+    {
+      n=n/10;
+      count++;
+    }
+    System.out.println(count);
+}
+}
